cb0e743284939f01852df7c8938345f9c6248c8e
Arcanum <c5h11n2o2p@gmail.com>
2015-05-07 01:42:13 +0200

    Update gdt.s

--- kernel/gdt.s
+++ kernel/gdt.s
@@ -1,16 +1,16 @@
 global gdt_flush
 
 gdt_flush:
-	mov eax, [esp + 4]
+	mov eax, [esp + 4] ; on récupère le pointeur de la GDT
 	lgdt [eax]
 	
-	mov ax, 0x10
+	mov ax, 0x10 ; on update les segments 
 	mov ds, ax
 	mov es, ax
 	mov fs, ax
 	mov gs, ax
 	mov ss, ax
 	
-	jmp 0x08:reload
+	jmp 0x08:reload ; recharge CS
 reload:
 	ret
